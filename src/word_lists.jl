f26afc506ab8887565d706554246ef616fc7a19e
John Myles White <jmw@johnmyleswhite.com>
2013-05-24 18:10:31 -0400

    Replace strcat with string

--- src/word_lists.jl
+++ src/word_lists.jl
@@ -2,7 +2,7 @@ for f in (:articles, :indefinite_articles, :definite_articles,
           :prepositions, :pronouns, :stopwords)
   @eval begin
     function ($f){T <: Language}(l::Type{T})
-      filename = Pkg.dir("Languages", "data", string($(f)), strcat(string(l), ".txt"))
+      filename = Pkg.dir("Languages", "data", string($(f)), string(string(l), ".txt"))
       return fetch_word_list(filename)
     end
   end
