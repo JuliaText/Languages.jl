af3f463823749d2a824b32c89d7beeb444f46075
Avik Sengupta <avik@sengupta.net>
2016-09-03 20:56:02 +0100

    fix 0.5 deprecations

--- src/utils.jl
+++ src/utils.jl
@@ -1,12 +1,12 @@
 const _cache = Dict{AbstractString,Array}()
 
 function fetch_word_list(filename::AbstractString)
-  haskey(_cache, filename) && return _cache[filename] 
+  haskey(_cache, filename) && return _cache[filename]
   try
     io = open(filename, "r")
     words = map(x -> chomp(x), readlines(io))
     close(io)
-    ret = convert(Array{UTF8String,1}, words)
+    ret = convert(Array{Compat.UTF8String,1}, words)
     _cache[filename] = ret
     return ret
   catch
